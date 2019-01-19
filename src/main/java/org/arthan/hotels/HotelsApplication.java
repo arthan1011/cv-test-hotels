02d251b26da7e4c26c957908e4f714e94d8725bc
BrainBurns <brainburns@protonmail.com>
2019-01-19 13:30:33 +0300

    initial commit

---
+++ src/main/java/org/arthan/hotels/HotelsApplication.java
@@ -0,0 +1,14 @@
+package org.arthan.hotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsApplication.class, args);
+	}
+
+}
+
